2f38c56edba7a358de98d32def36275d03aed585
Diawel <38136957+diawel@users.noreply.github.com>
2024-01-31 16:28:11 +0900

    印刷対応

--- src/components/Menu/index.css.ts
+++ src/components/Menu/index.css.ts
@@ -14,6 +14,11 @@ export const container = style({
   width: '100%',
   height: 0,
   overflow: 'visible',
+  '@media': {
+    print: {
+      display: 'none',
+    },
+  },
 })
 
 const backgroundBase = style({
